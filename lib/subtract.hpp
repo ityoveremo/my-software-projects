e89c09c8c5aac96b6a7fd93e6d7c78e532edc55c
Makavellidiablo <ityoveremo@gmail.com>
2022-09-10 17:40:19 +0100

    added some more changes to the files

---
+++ lib/subtract.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+/**
+ * @brief 
+ * this is a generic function that returns the value of the subraction of the parameters
+ * @tparam HELLO 
+ * --this is the generic typename,it can either be a float or an integer value
+ * @param firstNumber 
+ * this is a symbol that points to the address of the firstNumber
+ * @param secondNumber 
+ * this is a symbol that points to the address of the secondNumber
+ * @return HELLO 
+ * --if everything executes well, return the value of the subtraction of the parameters will return 
+ */
+namespace initial{
+    template<typename HELLO>
+    HELLO subtract(HELLO firstNumber, HELLO secondNumber) {
+
+        return firstNumber - secondNumber;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include "subtract.hpp"
+#include "add.hpp"
 
+
+
+/**
+ * @brief 
+ * this is the main function of the application it is here for pure cruise so dont trip
+ * @param argc 
+ * the variable holds the command line arguments for the application
+ * @param argv 
+ * the variable holds the number of arguments for the application
+ * @return int
+ * --if the code executes successfully the function returns zero. Otherwise it will return minus one
+ */
 int main(int argc, char *argv[])
 {
+
 	std::cout << "Hello world!" << std::endl;
+	std::cout<<"Hello my brothers and sisters,how una de do for this place"<<std::endl;
+	std::cout << "when the storm calms and the dust settles,only the real gon be here to talk the talk.if you know what i mean,man." << std::endl;
+	
+	return 0;
 }
