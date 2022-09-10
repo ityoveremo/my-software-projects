de220d9077a3ae7ec59a6de3d86320f80b8dba47
Makavellidiablo <ityoveremo@gmail.com>
2022-09-10 23:10:24 +0100

    What can i say??? Made some changes, i guess.....

---
+++ lib/add.hpp
@@ -0,0 +1,8 @@
+#ifndef DEA0D400_F947_490C_8358_414087E6D274
+#define DEA0D400_F947_490C_8358_414087E6D274
+
+#include <iostream>
+#include <fstream>
+
+
+#endif /* DEA0D400_F947_490C_8358_414087E6D274 */
